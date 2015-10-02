3a2a5a09fbab87e4229654e3498b153d78d7dc3c
andrewthedev <s4ncin@gmail.com>
2015-10-02 14:03:08 -0400

    Added more funactionality

--- Project1 Horvat Francis/Form1.cs
+++ Project1 Horvat Francis/Form1.cs
@@ -27,22 +27,41 @@ namespace Project1_Horvat_Francis
         {
             InitializeComponent();
             wordList = List;
-            curList = new List<string>();
+            curList = new List<string>();            
+        }
 
-            for (int i = 0; i < List.Count; i++)
+        // This function will reset the entire list of words into the
+        // list box by filling in the listbox with elements
+        private void initializeList()
+        {
+            // First check if the listbox has items
+            
+            if (gameWordList.Items.Count > 0)
             {
-                gameWordList.Items.Add(List.ElementAt(i));
+                // clear list
+                gameWordList.Items.Clear();
             }
+
+            // Fill in the list box
+
+            for (int i = 0; i < wordList.Count; i++)
+            {
+                gameWordList.Items.Add(wordList.ElementAt(i));
+            }
+
         }
 
 
 
         private void startButton_Click(object sender, EventArgs e)
         {
-            if (this.startButton.Text == "GO!") { 
+            if (this.startButton.Text == "GO!") {
+                // Fill in the list box
+            initializeList();
             getLetters();
             timer1.Enabled = true;
             this.startButton.Text = "Stop!";
+                
             }
 
             else
@@ -57,11 +76,72 @@ namespace Project1_Horvat_Francis
         private void endButton_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
+            // reset the game
+
+            gameWordList.Items.Clear();
+            resetAllComponents();
+        }
+
+        private void resetAllComponents()
+        {
+            // Set the button to "Go"
+            startButton.Text = "GO!";
+            // Set the reset the entire list
+            Letter_1.Text = "@";
+            Letter_2.Text = "@";
+            Letter_3.Text = "@";
+            Letter_4.Text = "@";
+            Letter_5.Text = "@";
+
+            // clear all button colors
+            Letter_1.BackColor = Color.Empty;
+            Letter_2.BackColor = Color.Empty;
+            Letter_3.BackColor = Color.Empty;
+            Letter_4.BackColor = Color.Empty;
+            Letter_5.BackColor = Color.Empty;
+
+            // clear all words
+            gameWordList.Items.Clear();
+
+            // Make buttons clickable again
+            checkBox1.Enabled = true;
+            checkBox2.Enabled = true;
+            checkBox3.Enabled = true;
+            checkBox4.Enabled = true;
+            checkBox5.Enabled = true;
+
+
+            // Clear the check boxes
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+
+
+
+
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            getLetters();
+            // If there is only one word left in the list then fill it;
+            if (gameWordList.Items.Count == 1)
+            {
+                string word = gameWordList.Items[0].ToString();
+                Letter_1.Text = Char.ToString(word[0]);
+                Letter_2.Text = Char.ToString(word[1]);
+                Letter_3.Text = Char.ToString(word[2]);
+                Letter_4.Text = Char.ToString(word[3]);
+                Letter_5.Text = Char.ToString(word[4]);
+
+            } else
+            {
+                getLetters();
+            }
+
         }
 
 
@@ -101,29 +181,37 @@ namespace Project1_Horvat_Francis
             return Char.ToUpper(let);
         }
 
+
+        // REFACTOR Checkbox logic if time permits.
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             searchWords(1);
+            checkBox1.Enabled = false;
         }
-
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
             searchWords(2);
+            checkBox2.Enabled = false;
         }
-
         private void checkBox3_CheckedChanged(object sender, EventArgs e)
         {
             searchWords(3);
+            checkBox3.Enabled = false;
         }
-
         private void checkBox4_CheckedChanged(object sender, EventArgs e)
         {
             searchWords(4);
+            checkBox4.Enabled = false;
         }
-
         private void checkBox5_CheckedChanged(object sender, EventArgs e)
         {
             searchWords(5);
+            checkBox5.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resetAllComponents();
         }
 
         public void searchWords(int position)
@@ -136,7 +224,25 @@ namespace Project1_Horvat_Francis
             four = Letter_4.Text[0];
             five = Letter_5.Text[0];
 
+            
+
+            List<string> reloadList = new List<string>();
+
+            // Save the previous state of the game list
+            for (int i = gameWordList.Items.Count - 1; i > -1; i--)
+            {
+                reloadList.Add(gameWordList.Items[i].ToString());
+            }
+
+
+
+                // variable to keep track of the count of matching words found;
+                int count = 0;
+
             switch (position) {
+
+               
+
                 case 1:
                     
                     for (int i =  gameWordList.Items.Count - 1; i > -1; i-- )
@@ -144,11 +250,32 @@ namespace Project1_Horvat_Francis
                         if ( gameWordList.Items[i].ToString()[0] == one )
                         {
                             Letter_1.BackColor = System.Drawing.Color.Green;
+                            count++;
                         }
                         else
                         {
+                                           
+
                             gameWordList.Items.Remove(gameWordList.Items[i].ToString());
+
+
                         }
+
+                        // If no words were found make the box red
+                        // reload the previous list as well
+                        
+                    }
+
+                    if (count <= 0)
+                    {
+                        gameWordList.Items.Clear();
+                        Letter_1.BackColor = System.Drawing.Color.Red;
+                        for (int j = 0; j < reloadList.Count; j++)
+                        {
+                            if (reloadList.Count > 0)
+                                gameWordList.Items.Add(reloadList.ElementAt(j));
+                        }
+
                     }
                     break;
 
@@ -159,11 +286,32 @@ namespace Project1_Horvat_Francis
                         if (gameWordList.Items[i].ToString()[1] == two)
                         {
                             Letter_2.BackColor = System.Drawing.Color.Green;
+                            count++;
                         }
                         else
                         {
+
+
                             gameWordList.Items.Remove(gameWordList.Items[i].ToString());
+
+
                         }
+
+                        // If no words were found make the box red
+                        // reload the previous list as well
+
+                    }
+
+                    if (count <= 0)
+                    {
+                        gameWordList.Items.Clear();
+                        Letter_2.BackColor = System.Drawing.Color.Red;
+                        for (int j = 0; j < reloadList.Count; j++)
+                        {
+                            if (reloadList.Count > 0)
+                                gameWordList.Items.Add(reloadList.ElementAt(j));
+                        }
+
                     }
                     break;
 
@@ -174,11 +322,32 @@ namespace Project1_Horvat_Francis
                         if (gameWordList.Items[i].ToString()[2] == three)
                         {
                             Letter_3.BackColor = System.Drawing.Color.Green;
+                            count++;
                         }
                         else
                         {
+
+
                             gameWordList.Items.Remove(gameWordList.Items[i].ToString());
+
+
                         }
+
+                        // If no words were found make the box red
+                        // reload the previous list as well
+
+                    }
+
+                    if (count <= 0)
+                    {
+                        gameWordList.Items.Clear();
+                        Letter_3.BackColor = System.Drawing.Color.Red;
+                        for (int j = 0; j < reloadList.Count; j++)
+                        {
+                            if (reloadList.Count > 0)
+                                gameWordList.Items.Add(reloadList.ElementAt(j));
+                        }
+
                     }
                     break;
 
@@ -189,11 +358,32 @@ namespace Project1_Horvat_Francis
                         if (gameWordList.Items[i].ToString()[3] == four)
                         {
                             Letter_4.BackColor = System.Drawing.Color.Green;
+                            count++;
                         }
                         else
                         {
+
+
                             gameWordList.Items.Remove(gameWordList.Items[i].ToString());
+
+
                         }
+
+                        // If no words were found make the box red
+                        // reload the previous list as well
+
+                    }
+
+                    if (count <= 0)
+                    {
+                        gameWordList.Items.Clear();
+                        Letter_4.BackColor = System.Drawing.Color.Red;
+                        for (int j = 0; j < reloadList.Count; j++)
+                        {
+                            if (reloadList.Count > 0)
+                                gameWordList.Items.Add(reloadList.ElementAt(j));
+                        }
+
                     }
                     break;
 
@@ -204,11 +394,32 @@ namespace Project1_Horvat_Francis
                         if (gameWordList.Items[i].ToString()[4] == five)
                         {
                             Letter_5.BackColor = System.Drawing.Color.Green;
+                            count++;
                         }
                         else
                         {
+
+
                             gameWordList.Items.Remove(gameWordList.Items[i].ToString());
+
+
                         }
+
+                        // If no words were found make the box red
+                        // reload the previous list as well
+
+                    }
+
+                    if (count <= 0)
+                    {
+                        gameWordList.Items.Clear();
+                        Letter_5.BackColor = System.Drawing.Color.Red;
+                        for (int j = 0; j < reloadList.Count; j++)
+                        {
+                            if (reloadList.Count > 0)
+                                gameWordList.Items.Add(reloadList.ElementAt(j));
+                        }
+
                     }
                     break;
 
